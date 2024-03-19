2a4b74852a82b0256ec9e0b9d6644b9389de3bd0
Kdzio <korix000@gmail.com>
2024-03-19 20:09:32 +0100

    Add files via upload

---
+++ php_01_widok_kontroler/app/calc.php
@@ -0,0 +1,70 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+
+$kwota = $_REQUEST ['kwota'];
+$lata = $_REQUEST ['lata'];
+$oprocentowanie = $_REQUEST ['oprocentowanie'];
+
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// sprawdzenie, czy parametry zostały przekazane
+if ( ! (isset($kwota) && isset($lata) && isset($oprocentowanie))) {
+	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+if ( $kwota == "") {
+	$messages [] = 'Nie podano kwoty';
+}
+if ( $lata == "") {
+	$messages [] = 'Nie podano na ile lat';
+}
+if ( $lata == "") {
+	$messages [] = 'Nie podano Oprocentowania';
+}
+
+
+if (empty( $messages )) {
+	
+	// sprawdzenie, czy $kwota i $y są liczbami całkowitymi
+	if (! is_numeric( $kwota )) {
+		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $lata )) {
+		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+	}
+	if (! is_numeric( $oprocentowanie )) {
+		$messages [] = 'Trzecia wartość nie jest liczbą całkowitą';
+	}	
+
+}
+
+// 3. wykonaj zadanie jeśli wszystko w porządku
+
+if (empty ( $messages )) { // gdy brak błędów
+	
+	//konwersja parametrów na int
+	$kwota = intval($kwota);
+	$lata = intval($lata);
+	$oprocentowanie = intval($oprocentowanie);
+	
+	//wykonanie operacji
+	$odsetki = ($kwota * $oprocentowanie / 100);
+	$do_splaty = $kwota + $odsetki;
+	$liczba_rat = $lata * 12;
+	$miesieczna_rata = $do_splaty / $liczba_rat;
+	$result = round($miesieczna_rata,2);
+	
+}
+
+include 'calc_view.php';
---
+++ php_01_widok_kontroler/app/calc_view.php
@@ -0,0 +1,44 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator</title>
+</head>
+<body>
+
+<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+	<label for="id_kwota">Kwota: </label>
+	<input id="id_kwota" type="text" name="kwota" value="<?php isset($kwota)?print($kwota):'Wpisz Liczbe'; ?>" /><br />
+    
+	<label for="id_lata">Ile lat?: </label>
+	<input id="id_lata" type="text" name="lata" value="<?php isset($lata)?print($lata):'Wpisz Liczbe'; ?>" /><br />
+	
+	<label for="id_lata">Oprocentowanie: </label>
+	<input id="id_lata" type="text" name="oprocentowanie" value="<?php isset($oprocentowanie)?print($oprocentowanie):'Wpisz Liczbe'; ?>" /><br />
+	</br>
+	<input type="submit" value="Oblicz" />
+</form>	
+
+
+<?php
+//wyświeltenie listy błędów, jeśli istnieją
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($result)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<?php echo 'Wynik: '.$result; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
