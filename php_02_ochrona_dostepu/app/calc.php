227a897f882d6aca6ffd46e6b90c317c2f6adcea
Kdzio <korix000@gmail.com>
2024-03-20 18:32:42 +0100

    Add files via upload

---
+++ php_02_ochrona_dostepu/app/calc.php
@@ -0,0 +1,93 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+
+// KONTROLER strony kalkulatora
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+//ochrona kontrolera - poniższy skrypt przerwie przetwarzanie w tym punkcie gdy użytkownik jest niezalogowany
+include _ROOT_PATH.'/app/security/check.php';
+
+//pobranie parametrów
+function getParams(&$kwota,&$lata,&$oprocentowanie){
+	$kwota = isset($_REQUEST['kwota']) ? $_REQUEST['kwota'] : null;
+	$lata = isset($_REQUEST['lata']) ? $_REQUEST['lata'] : null;
+	$oprocentowanie = isset($_REQUEST['oprocentowanie']) ? $_REQUEST['oprocentowanie'] : null;	
+}
+
+//walidacja parametrów z przygotowaniem zmiennych dla widoku
+function validate(&$kwota,&$lata,&$oprocentowanie,&$messages){
+	// sprawdzenie, czy parametry zostały przekazane
+	if ( ! (isset($kwota) && isset($lata) && isset($oprocentowanie))) {
+		// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+		// teraz zakładamy, ze nie jest to błąd. Po prostu nie wykonamy obliczeń
+		return false;
+	}
+
+	// sprawdzenie, czy potrzebne wartości zostały przekazane
+if ( $kwota == "") {
+	$messages [] = 'Nie podano kwoty';
+}
+if ( $lata == "") {
+	$messages [] = 'Nie podano na ile lat';
+}
+if ( $oprocentowanie == "") {
+	$messages [] = 'Nie podano oprocentowania';
+}
+if (count ( $messages ) != 0) return false;
+	
+	// sprawdzenie, czy $kwota i $y są liczbami całkowitymi
+	if (! is_numeric( $kwota )) {
+		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $lata )) {
+		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+	}
+	if (! is_numeric( $oprocentowanie )) {
+		$messages [] = 'Trzecia wartość nie jest liczbą całkowitą';
+	}	
+
+	if (count ( $messages ) != 0) return false;
+	else return true;
+}
+
+
+function process(&$kwota,&$lata,&$oprocentowanie,&$messages,&$result){
+	global $role;
+	
+	//konwersja parametrów na int
+	$kwota = intval($kwota);
+	$lata = intval($lata);
+	$oprocentowanie = intval($oprocentowanie);
+	
+	//wykonanie operacji
+	
+			if ($role == 'admin'){
+				$odsetki = ($kwota * $oprocentowanie / 100);
+				$do_splaty = $kwota + $odsetki;
+				$liczba_rat = $lata * 12;
+				$miesieczna_rata = $do_splaty / $liczba_rat;
+				$result = round($miesieczna_rata,2);
+			} else {
+				$messages [] = 'Tylko administrator może wyliczyć miesieczna rate !';
+			}	
+}
+
+//definicja zmiennych kontrolera
+$kwota = null;
+$lata = null;
+$oprocentowanie = null;
+$result = null;
+$messages = array();
+
+//pobierz parametry i wykonaj zadanie jeśli wszystko w porządku
+getParams($kwota,$lata,$oprocentowanie);
+if ( validate($kwota,$lata,$oprocentowanie,$messages) ) { // gdy brak błędów
+	process($kwota,$lata,$oprocentowanie,$messages,$result);
+}
+
+
+include 'calc_view.php';
